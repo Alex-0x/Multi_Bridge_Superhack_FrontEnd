90ee136efcdb995994571b79f24a2bece80f6878
Alex-0x <alex.mignemi@gmail.com>
2023-08-08 21:15:41 +0200

    new pages

---
+++ multi_bridge_fe/src/app/dashboard/fund/page.tsx
@@ -0,0 +1,15 @@
+import AccFund from "../../../components/AccFund";
+import NavHome from "../../../components/NavHome";
+
+export default function Page(){
+    return(
+        <div>
+            <NavHome/>
+            <br />
+        <h1>Add funds to the contract</h1>
+            <br />
+            <AccFund/>
+        </div>
+        
+    )
+}
---
+++ multi_bridge_fe/src/app/dashboard/mint/page.tsx
@@ -0,0 +1,15 @@
+import NavHome from "../../../components/NavHome";
+import MintCcip from "../../../components/mintCcip";
+
+
+export default function MintPage(){
+    return (
+        <div>
+        <NavHome/>
+        <br />
+        <h1>Mint / swap ccip</h1>
+        <br />
+        <MintCcip/>
+        </div>
+    )
+}
