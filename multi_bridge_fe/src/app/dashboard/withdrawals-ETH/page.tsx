322855a41f86ef982ad2f3a113a509b5578993ac
Alex-0x <alex.mignemi@gmail.com>
2023-08-12 17:08:22 +0200

    improved content and usability

--- multi_bridge_fe/src/app/dashboard/withdrawals-ETH/page.tsx
+++ multi_bridge_fe/src/app/dashboard/withdrawals-ETH/page.tsx
@@ -1,73 +1,73 @@
+'use client'
 import NavHome from "../../../components/NavHome";
 import { NetworkSwitcher } from "../../../components/NetworkSwitcher";
-
-import { Allowance_ccipToken_Mumbai } from "../../../components/allowanceCcipToken/allowance_mumbai";
 import { Allowance_ccipToken_Sepolia_from_mumbai } from "../../../components/allowanceCcipToken/allowance_sepolia_from_mumbai";
 import { Deposit_for_wit_ETH_Mumbai } from "../../../components/withdrawals_ETH/deposit_for_wit_Sep_Mumb";
 import { Deposit_for_wit_ETH_OP } from "../../../components/withdrawals_ETH/deposit_for_wit_Sepolia";
 import { Wihdrawals_ETH_Mumbai } from "../../../components/withdrawals_ETH/with_sep_mumb";
 import { Wihdrawals_ETH_OP } from "../../../components/withdrawals_ETH/withdrawals_Sepolia";
+import "../../../assets/styles/withdrawalsETHPage.css"; 
+import { useState } from "react";
+import { Allowance_ccipToken_Sepolia_from_Optimism } from "../../../components/allowanceCcipToken/allowance_ccip_op-sepolia";
 
-
-export default function WithdrawalsPage_ETH(){
+export default function WithdrawalsPage_ETH() {
+    const [activeSection, setActiveSection] = useState("op"); // Sezione attualmente attiva
+  
+    const handleSectionClick = (sectionId: string) => {
+      setActiveSection(sectionId);
+    };
+  
     return (
-        <div>
-        <NavHome/>
+      <div>
+        <NavHome />
         <br />
-        <NetworkSwitcher/>
+        <NetworkSwitcher />
         <br />
-        <h1>Withdrawals ETH</h1>
-        <br />
-
-        <h2>Withdrawals ETH you come from Optimism</h2>
-        <br />
-           
-          <h3>3..Now yoy can chain network switch from Optimism to Sepolia</h3>
+        <div className="container">
+          <h1 id="title">Withdrawals ETH</h1>
           <br />
-             <br />
-            <NetworkSwitcher/>
-            <br />
-          <div>
-          <Deposit_for_wit_ETH_OP/>
+  
+          <div className="menu">
+            <div
+              className={`menu-item ${activeSection === "op" ? "active" : ""}`}
+              onClick={() => handleSectionClick("op")}
+            >
+              Optimism
+            </div>
+            <div
+              className={`menu-item ${activeSection === "mumb" ? "active" : ""}`}
+              onClick={() => handleSectionClick("mumb")}
+            >
+              Mumbai
+            </div>
+            <div
+              className={`menu-item ${activeSection === "arb" ? "active" : ""}`}
+              onClick={() => handleSectionClick("arb")}
+            >
+              Arbitrum
+            </div>
           </div>
-          <div>
-          <Wihdrawals_ETH_OP/>
-          </div>
+  
+         
+          <section id="op" className={`section ${activeSection === "op" ? "active" : ""}`}>
+          <Allowance_ccipToken_Sepolia_from_Optimism/>
+          <Deposit_for_wit_ETH_OP />
+              <Wihdrawals_ETH_OP />
+          </section>
+  
+      
+          <section id="mumb" className={`section ${activeSection === "mumb" ? "active" : ""}`}>
+          <Allowance_ccipToken_Sepolia_from_mumbai />
+              <Deposit_for_wit_ETH_Mumbai />
+              <Wihdrawals_ETH_Mumbai />
+          </section>
+  
+       
+          <section id="arb" className={`section ${activeSection === "arb" ? "active" : ""}`}>
+            {/* ... */}
+          </section>
+        </div>
+      </div>
+    );
+  }
 
-
-          <br />
-          <h2>Withdrawals ETH you come from Mumbai</h2>
-        <br />
-           
-          <h3>3..Now yoy can chain network switch from Mumbai to Sepolia</h3>
-          <br />
-          <br />
-            <NetworkSwitcher/>
-            <br />
-          <h3>4..approve spender contract with allowance:</h3>
-          <br />
-          <Allowance_ccipToken_Sepolia_from_mumbai/>
-           <br />
-          <div>
-          <Deposit_for_wit_ETH_Mumbai/>
-          </div>
-          <div>
-          <Wihdrawals_ETH_Mumbai/>
-          </div>
-
-          <br />
-          <h2>Withdrawals ETH you come from Arbitrum</h2>
-        <br />
-           
-          <h3>3..Now yoy can chain network switch from Arbitrum to Sepolia</h3>
-          <br />
-          <br />
-            <NetworkSwitcher/>
-            <br />
-          <h3>4..approve spender contract with allowance:</h3>
-          <br />
-
-
-</div>
-        )
-}
--- multi_bridge_fe/src/app/dashboard/withdrawals-WTETH/page.tsx
+++ multi_bridge_fe/src/app/dashboard/withdrawals-WTETH/page.tsx
@@ -1,79 +1,72 @@
+'use client'
+
+import { useState } from "react";
 import NavHome from "../../../components/NavHome";
 import { NetworkSwitcher } from "../../../components/NetworkSwitcher";
 import { Allowance_ccipToken_Arbitrum } from "../../../components/allowanceCcipToken/allowance_arbitrum";
 import { Allowance_ccipToken_Mumbai } from "../../../components/allowanceCcipToken/allowance_mumbai";
 import { Allowance_ccipToken_Op } from "../../../components/allowanceCcipToken/allowance_op";
-import { MintCcipSepolia } from "../../../components/deposit/mint_ccip";
-import MintCcip from "../../../components/mintCcip";
-import { Deposit_for_wit_ETH_OP } from "../../../components/withdrawals_ETH/deposit_for_wit_Sepolia";
-import { Wihdrawals_ETH_OP } from "../../../components/withdrawals_ETH/withdrawals_Sepolia";
 import { WihdrawalsWTETH_Arbitrum } from "../../../components/withdrawals_WTETH/withdrawals_Arbitrum";
 import { WihdrawalsWTETH_Mumbai } from "../../../components/withdrawals_WTETH/withdrawals_Mumbai";
 import { WihdrawalsWTETH_OP } from "../../../components/withdrawals_WTETH/withdrawals_Optimism";
-
+import "../../../assets/styles/withdrawalsETHPage.css"; 
 
 export default function WithdrawalsPageWTETH(){
+    const [activeSection, setActiveSection] = useState("op"); // Sezione attualmente attiva
+  
+    const handleSectionClick = (sectionId: string) => {
+      setActiveSection(sectionId);
+    };
+  
     return (
-        <div>
-        <NavHome/>
+      <div>
+        <NavHome />
         <br />
-        <NetworkSwitcher/>
+        <NetworkSwitcher />
         <br />
-        <h1>Withdrawals WTETH</h1>
-        <br />
-        
-        
-        
-        <h2>Withrowals WTETH in Optimism</h2>
-        <br />
-        <h1>remaind to switch network in Optimism</h1>
-        <br />
-        <p>3..add in you metamask WTETH: 0xbd59412cb6668e7a17c99c19108893437cc84f01 </p>
-        <br />
-        <h2>4..You have to give approve to spend Token Ccip in Optimism chain</h2>
-        <Allowance_ccipToken_Op/>
-        <br />
-        <h2>ad now..Withdrawals WTEHT in Optimism</h2>
-        <WihdrawalsWTETH_OP/>
-        
-        
-        <br />
-        <h2>Withrowals WTETH in Mumbai</h2>
-        <br />
-
-        <h4>3..switch network from Sepolia to Mumbai</h4>
-            <br />
-            <p>4..add in you metamask WTETH: 0x8ec150b21b2644cfc4f9fff3ead6061e12bcf9e8</p>
+        <div className="container">
+          <h1 id="title">Withdrawals WTETH</h1>
           <br />
-        <h2>5..You have to give approve to spend Token Ccip in Mumbai chain</h2>
-        <Allowance_ccipToken_Mumbai/>
-        <br />
-        <h2>ad now..Withdrawals WTEHT in Mumbai</h2>
-        <WihdrawalsWTETH_Mumbai/>
-
-
-        <br />
-        <h2>Withrowals WTETH in Arbitrum</h2>
-        <br />
-        <h4>3..switch network from Sepolia to Arbitrum</h4>
-            <br />
-            <p>4..add in you metamask WTETH: 0x73dedf5607b25ef415be12de7a044013dd53133a </p>
-          <br />
-        <h2>5..You have to give approve to spend Token Ccip in Arbitrum chain</h2>
-        <Allowance_ccipToken_Arbitrum/>
-        <br />
-        <h2>ad now..Withdrawals WTEHT in Arbitrum</h2>
-
-        <WihdrawalsWTETH_Arbitrum/>
-
-
-
-
+  
+          <div className="menu">
+            <div
+              className={`menu-item ${activeSection === "op" ? "active" : ""}`}
+              onClick={() => handleSectionClick("op")}
+            >
+              Optimism
+            </div>
+            <div
+              className={`menu-item ${activeSection === "mumb" ? "active" : ""}`}
+              onClick={() => handleSectionClick("mumb")}
+            >
+              Mumbai
+            </div>
+            <div
+              className={`menu-item ${activeSection === "arb" ? "active" : ""}`}
+              onClick={() => handleSectionClick("arb")}
+            >
+              Arbitrum
+            </div>
+          </div>
+  
+         
+          <section id="op" className={`section ${activeSection === "op" ? "active" : ""}`}>
+          <Allowance_ccipToken_Op/>
+          <WihdrawalsWTETH_OP/>
+          </section>
+  
+      
+          <section id="mumb" className={`section ${activeSection === "mumb" ? "active" : ""}`}>
+          <Allowance_ccipToken_Mumbai/>
+          <WihdrawalsWTETH_Mumbai/>
+          </section>
+  
+       
+          <section id="arb" className={`section ${activeSection === "arb" ? "active" : ""}`}>
+          <Allowance_ccipToken_Arbitrum/>
+          <WihdrawalsWTETH_Arbitrum/>
+          </section>
         </div>
-
-        
-
-
-        
-    )
+      </div>
+    );
 }
