082e78f881ee5b8a59e11ecc23a7ccdc600d76f8
Alex-0x <alex.mignemi@gmail.com>
2023-08-12 17:11:21 +0200

    unnecessary component

--- multi_bridge_fe/src/components/buttonFund.tsx
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css'
-
-export function Button() {
-
-    return (
-      <a className="btn btn-primary" href="/dashboard/fund" role="button">Bridge</a>
-    )
-}
