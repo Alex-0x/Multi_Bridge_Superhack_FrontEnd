b8f17b01eadca7e1f0924ad83e790751ba5f382f
Alex-0x <alex.mignemi@gmail.com>
2023-08-12 17:12:10 +0200

    changed layout to Dropdowns menu

--- multi_bridge_fe/src/components/Connect.tsx
+++ multi_bridge_fe/src/components/Connect.tsx
@@ -1,34 +1,37 @@
 'use client'
-
-import { BaseError } from 'viem'
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { Dropdown, Button } from 'react-bootstrap';
+import { BaseError } from 'viem';
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
 
 export function Connect() {
-  const { connector, isConnected } = useAccount()
+  const { connector, isConnected } = useAccount();
   const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect()
-  const { disconnect } = useDisconnect()
+    useConnect();
+  const { disconnect } = useDisconnect();
 
   return (
-    <div>
-      <div>
-        {isConnected && (
-          <button onClick={() => disconnect()}>
-            Disconnect from {connector?.name}
-          </button>
-        )}
-
-        {connectors
-          .filter((x) => x.ready && x.id !== connector?.id)
-          .map((x) => (
-            <button key={x.id} onClick={() => connect({ connector: x })}>
-              {x.name}
-              {isLoading && x.id === pendingConnector?.id && ' (connecting)'}
-            </button>
-          ))}
-      </div>
-
+    <Dropdown>
+      <Dropdown.Toggle variant="secondary" id="connectDropdown">
+        {isConnected ? `Connected to ${connector?.name}` : "Connect"}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {connectors.map((x) => (
+          <Dropdown.Item
+            key={x.id}
+            onClick={() => connect({ connector: x })}
+            disabled={isLoading && x.id === pendingConnector?.id}
+          >
+            {x.name}
+            {isLoading && x.id === pendingConnector?.id && " (connecting)"}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+      {isConnected && (
+        <Button variant="light" onClick={() => disconnect()}>
+          Disconnect from {connector?.name}
+        </Button>
+      )}
       {error && <div>{(error as BaseError).shortMessage}</div>}
-    </div>
-  )
+    </Dropdown>
+  );
 }
